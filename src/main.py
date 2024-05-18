dac850fcf89abb981d694d25d553eb6c4b8b9c90
Mohammad Attari <masoud.ata@gmail.com>
2024-05-18 21:33:38 +0200

    add input assembly file parameter option

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-x", "--hex", help="Output in hex.", action="store_true")
+    parser.add_argument("-i", "--input", help="Input assembly file name.")
     args = parser.parse_args()
-    as_hex = args.hex
+    input_file = args.input if args.input else '../examples/tryouts.s'
 
-    binary_code, hex_code = assemble("../examples/tryouts.s")
+    binary_code, hex_code = assemble(input_file)
 
     with open("../output/out_binary.txt", 'w') as out_binary_file:
         out_binary_file.write(binary_code)
