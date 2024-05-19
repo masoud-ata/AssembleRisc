aedd7967fc94d404a3db434d74e426482f35d606
Mohammad Attari <masoud.ata@gmail.com>
2024-05-19 19:10:42 +0200

    some refactoring

--- src/main.py
+++ src/main.py
@@ -2,18 +2,26 @@ from assembler.assemblerisc import assemble
 import argparse
 
 
-def main():
+def get_args() -> str:
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Input assembly file name.")
     args = parser.parse_args()
     input_file = args.input if args.input else '../examples/tryouts.s'
+    return input_file
+
+
+def write_output_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+
+
+def main():
+    input_file = get_args()
 
     binary_code, hex_code = assemble(input_file)
 
-    with open("../output/out_binary.txt", 'w') as out_binary_file:
-        out_binary_file.write(binary_code)
-    with open("../output/out_hexadeciaml.txt", 'w') as out_hex_file:
-        out_hex_file.write(hex_code)
+    write_output_file('../output/out_binary.txt', binary_code)
+    write_output_file('../output/out_hexadeciaml.txt', hex_code)
 
 
 if __name__ == "__main__":
